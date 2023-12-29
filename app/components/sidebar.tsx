da118bae2583d2d4e64cf6313d2950572232c893
Amazingjoee <130811991+Amazingjoee@users.noreply.github.com>
2023-12-30 00:58:48 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           AMAZINGJOE
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          AMAZINGJOE'S AI assistant.
+          AMAZINGJOE AI assistant.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
